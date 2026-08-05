cefd53820fbd9fe332b3fcd529a9161c025853f9
Richard Liu <rl@seeingflow.com>
2026-08-05 22:19:23 +0800

    Add final document library route

--- resource-detail.js
+++ resource-detail.js
@@ -19,7 +19,7 @@ function normalizeCategory(value = "") {
 function redirectCategoryResource() {
   const category = normalizeCategory(resource.type);
   if (category === "documents") {
-    window.location.replace("document-library-filtered-20260805.html");
+    window.location.replace("document-library-final-20260805.html");
     return;
   }
   if (category === "clientcases" && resource.body) {
